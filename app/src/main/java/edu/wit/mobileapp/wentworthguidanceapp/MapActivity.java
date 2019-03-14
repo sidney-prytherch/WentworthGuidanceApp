52606798f405d1b5a9c57c4067e547be786f328f
prytherchs <sidneyprytherch@gmail.com>
2019-03-14 12:31:08 -0400

    Add searchbar and menubutton to mapview

--- app/src/main/java/edu/wit/mobileapp/wentworthguidanceapp/MapActivity.java
+++ app/src/main/java/edu/wit/mobileapp/wentworthguidanceapp/MapActivity.java
@@ -2,12 +2,15 @@ package edu.wit.mobileapp.wentworthguidanceapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MapActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide(); // hide the title bar
         setContentView(R.layout.activity_map);
     }
 }
